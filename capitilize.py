d2d615bbad3ac08d6529772a27d7fbd6f66f1fe0
juliet amaka <johnontita2000@gmail.com>
2024-11-03 19:25:27 +0300

    capitilizing the first letter of words

---
+++ capitilize.py
@@ -0,0 +1,10 @@
+"""
+5. Write a program that accepts a string as input, capitalizes the first letter of each
+word in the string, and then returns the result string.
+Examples:
+"hi"=> returns "Hi"
+"i love programming"=> returns "I Love Programming
+"""
+phrase=input('enter the string')
+phrase_capitalized=phrase.title()
+print(phrase_capitalized)
